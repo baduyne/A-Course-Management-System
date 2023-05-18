052094abcce75bf63660747b76e77788064e14dd
DUY NGUYỄN BÁ <22120078@student.hcmus.edu.vn>
2023-05-18 21:58:25 +0700

    add lib graphic

---
+++ A Course Management System/do_hoa.cpp
@@ -0,0 +1,41 @@
+﻿#include"do_hoa.h"
+using namespace std;
+
+
+
+// Hàm tô màu.
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau, x);
+}
+// Hàm lấy Key từ bàn phím
+/* GetAsyncKeyState(VK_ESCAPE) => bắt sự kiện phím Esc */
+
+// Hàm tự viết
+void ToMau(int x, int y, char* a, int color) // x, y là tọa độ con trỏ cần nhảy đến để viết, a là chuỗi cần truyền vào, color là màu truyền vào.
+{
+	gotoxy(x, y);
+	textcolor(color);
+	cout << a;
+	textcolor(7);
+}
+void gotoxy(int x, int y)
+{
+	static HANDLE h = NULL;
+	if (!h)
+		h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD c = { x, y };
+	SetConsoleCursorPosition(h, c);
+}
+
+void GoTo(int posX, int posY)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD Position;
+	Position.X = posX;
+	Position.Y = posY;
+
+	SetConsoleCursorPosition(hStdout, Position);
+}
---
+++ A Course Management System/do_hoa.h
@@ -0,0 +1,12 @@
+#ifndef do_hoa 
+#define do_hoa
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+void textcolor(int x);
+void ToMau(int x, int y, char* a, int color);
+void gotoxy(int x, int y);
+void GoTo(int posX, int posY);
+#endif
+
